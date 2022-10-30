c18acf7b083e195642bf201510b457c6e9bf5259
by-German <german280599@gmail.com>
2022-10-30 01:45:42 -0500

    feat: add navigation header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <login />  
+  <!-- <login />   -->
+  <Header />
 </template>
 
 <script>
-import Login from './components/Login.vue';
+// import Login from './components/Login.vue';
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    Login
+    // Login,
+    Header
   }
 }
 </script>
