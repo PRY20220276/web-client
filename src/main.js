d2631da4e72b4b426cbdef5536a691a0817cf6f2
by-German <german280599@gmail.com>
2022-10-30 00:13:36 -0500

    feat: add configuration for prime vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+
+// components for prime vue
+app.component('InputText', InputText)
+
+app.mount('#app')
