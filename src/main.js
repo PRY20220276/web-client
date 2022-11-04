57dfbcbef44c0758cb3407809776aa809efc5725
Dani237 <54338925+Dani237@users.noreply.github.com>
2022-11-03 23:13:48 -0500

    feat: first touch for step view

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
+import Steps from 'primevue/steps';
 import Button from 'primevue/button';
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
+
 
 const app = createApp(App);
 app.use(PrimeVue);
@@ -13,5 +15,7 @@ app.use(PrimeVue);
 // components for prime vue
 app.component('InputText', InputText);
 app.component('Button', Button);
+app.component('Steps', Steps);
+
 
 app.mount('#app')
