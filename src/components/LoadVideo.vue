d81272d995353cc4663de6d71f19443aaace8a5f
by-German <german280599@gmail.com>
2022-11-10 20:26:48 -0500

    feat: add steps component

---
+++ src/components/LoadVideo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="load-video">
+    load video
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import Router from "./router/router"
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import Chip from 'primevue/chip';
+import Steps from 'primevue/steps';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
 // styles
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
@@ -17,10 +20,13 @@ import 'primeicons/primeicons.css'                           //icons
 const app = createApp(App)
 app.use(PrimeVue)
 app.use(Router)
+app.use(ToastService);
 
 // components for prime vue
 app.component('InputText', InputText)
 app.component('Button', Button)
 app.component('Chip', Chip)
+app.component('Steps', Steps)
+app.component('Toast', Toast);
 
 app.mount('#app')
